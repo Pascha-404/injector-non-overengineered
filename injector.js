a7b53af7ff8c9bb45f6611504b9206b7133ef3d2
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-24 19:34:24 +0200

    fix: correct typo in stripNormalize method and restructure replaceTextContent function
    - Fixed typo in the regex pattern within the stripNormalize method
    
    - Reworked replaceTextContent function to use a for loop for better handling of missing keys

--- injector.js
+++ injector.js
@@ -2,14 +2,22 @@
 const translations = {/* TRANSLATIONS_JSON */};
 
 String.prototype.stripNormalize = function () {
-	return this.replace(/s\+/g, ' ').trim();
+	return this.replace(/\s+/g, ' ').trim();
 };
 
 const replaceTextContent = (selector, translations) => {
-	document.querySelectorAll(selector).forEach(element => {
+	document.querySelectorAll(selector).forEach((element, idx) => {
 		const originalText = element.innerHTML.stripNormalize();
-		if (translations[originalText].stripNormalize()) {
-			element.innerHTML = translations[originalText].stripNormalize();
+		let foundTranslation = null;
+
+		for (let key in translations) {
+			if (key.stripNormalize() === originalText) {
+				foundTranslation = translations[key].stripNormalize();
+				break;
+			}
+		}
+		if (foundTranslation) {
+			element.innerHTML = foundTranslation;
 		}
 	});
 };
