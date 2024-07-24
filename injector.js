533a7cd79e19ef21e21d994c2f56d42e4b55e147
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-24 18:50:19 +0200

    feat: add replaceTextContent function
    - Created function that utilizes the stripNormalize method to find matching content
    
    - Replaces matching content with the translated version

--- injector.js
+++ injector.js
@@ -4,3 +4,12 @@ const translations = {/* TRANSLATIONS_JSON */};
 String.prototype.stripNormalize = function () {
 	return this.replace(/s\+/g, ' ').trim();
 };
+
+const replaceTextContent = (selector, translations) => {
+	document.querySelectorAll(selector).forEach(element => {
+		const originalText = element.innerHTML.stripNormalize();
+		if (translations[originalText].stripNormalize()) {
+			element.innerHTML = translations[originalText].stripNormalize();
+		}
+	});
+};
