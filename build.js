1ae72f2c87245a7160a196a6c4c3a99cdad2fba7
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-24 18:30:06 +0200

    feat: extend build file
    - Added creation of dist folder to build file
    
    - Compiled files are now saved to the dist folder

--- build.js
+++ build.js
@@ -7,9 +7,16 @@ const translations = fs.readFileSync('translations.json', 'utf8');
 let jsFile = fs.readFileSync('injector.js', 'utf8');
 
 // Replace the placeholder with the actual translations
-jsFile = jsFile.replace('/* TRANSLATIONS_JSON */', translations);
+jsFile = jsFile.replace('{/* TRANSLATIONS_JSON */}', translations);
+
+// Check if dist directory exists
+// If !dist directory, create!
+if (!fs.existsSync('./dist')) {
+	fs.mkdirSync('dist');
+	console.log("Created 'dist' directory...");
+}
 
 // Write the combined content to a new file
-fs.writeFileSync('compiled.js', jsFile);
+fs.writeFileSync('./dist/compiled.js', jsFile);
 
 console.log('Compiled succesfully to compiled.js');
