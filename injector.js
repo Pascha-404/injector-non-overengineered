cc9efb46a207de6e88ca8b146541e7c83a3cce03
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-25 19:28:46 +0200

    feat: add function to replace dateTime content

--- injector.js
+++ injector.js
@@ -6,7 +6,7 @@ String.prototype.stripNormalize = function () {
 };
 
 const replaceTextContent = (selector, translations) => {
-	document.querySelectorAll(selector).forEach((element, idx) => {
+	document.querySelectorAll(selector).forEach(element => {
 		const originalText = element.innerHTML.stripNormalize();
 		let foundTranslation = null;
 
@@ -33,5 +33,26 @@ const replaceAttributes = (selector, attributes, translations) => {
 	});
 };
 
+const createDateTime = () => {
+	const now = new Date();
+	const day = String(now.getDate()).padStart(2, '0');
+	const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+	const year = now.getFullYear();
+	const hours = String(now.getHours()).padStart(2, '0'); // 24-hour format
+	const minutes = String(now.getMinutes()).padStart(2, '0');
+	const seconds = String(now.getSeconds()).padStart(2, '0');
+	return `${day}.${month}.${year}, ${hours}:${minutes}:${seconds}`;
+};
+
+const replaceDateTime = selector => {
+	document.querySelectorAll(selector).forEach(element => {
+		setInterval(() => {
+			element.innerHTML = createDateTime();
+		}, 1000);
+		element.setAttribute('id', `${element.getAttribute('id')}-updated`);
+	});
+};
+
+replaceDateTime('#current-date-time');
 replaceTextContent('h3,p,button', translations);
 replaceAttributes('button', ['data-soldout', 'data-adding', 'data-added'], translations);
